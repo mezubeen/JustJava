13515125326457399c2a2823ca1d3a329966d302
zubeen khan <zubeen95@gmail.com>
2017-07-05 00:42:18 +0530

    bug fixed

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.android.justjava;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,32 +27,61 @@ public class MainActivity extends AppCompatActivity {
     // This method is called when the order button is clicked.
 
     public void submitOrder(View view) {
-        int price = calculatePrice();
-        String priceMessage = createOrderSummary(price);
+        // FOR WHIPPED CREAM TOPPING
+        CheckBox whippedcreamcheckbox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        Boolean hasWhippedCream = whippedcreamcheckbox.isChecked();
+        // FOR CHOCOLATE TOPPING
+        CheckBox chocolateTopping = (CheckBox) findViewById(R.id.chocolate_topping_checkbox);
+        Boolean hasChocolateTopping = chocolateTopping.isChecked();
+
+        //FOR NAME
+        EditText name = (EditText) findViewById(R.id.name);
+        String name_string = name.getText().toString();
+
+        // Log.v("MainActivity", "has whippedcream "+hasWhippedCream);
+
+        int price = calculatePrice(hasWhippedCream, hasChocolateTopping);
+        String priceMessage = createOrderSummary(name_string, price, hasWhippedCream, hasChocolateTopping);
         // String priceMessage = "Total: $" + price ;
         //priceMessage = priceMessage + " \nThank you!";
-        displayMessage(priceMessage);
 
-//        displayPrice(quantity * 3);
+       // displayMessage(priceMessage);
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto","abc@gmail.com", null));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "");
+        startActivity(Intent.createChooser(emailIntent, "Send email..."));
+
+
 
     }
 
 
-    private int calculatePrice() {
-        int price = quantity * 5;
-        return price;
+    private int calculatePrice(Boolean addWhippedCream, Boolean addChocolateToping) {
+        int price = 5;
+
+        if(addWhippedCream){
+            price = price + 1;
+        }
+
+        if(addChocolateToping){
+            price = price +2;
+        }
+
+        return price * quantity;
     }
 
     //ORDER SUMMARY METHOD
-    private String createOrderSummary(int price){
-        String priceMessage =  "Name: Zubeen";
-        priceMessage = priceMessage + "\nQuantity: " +quantity;
+    private String createOrderSummary(String name, int price, boolean addWhippedTopping, boolean addChocolateTopping) {
+        String priceMessage = "Name: " + name;
+        priceMessage = priceMessage + "\nAdd Whipped Cream Topping? " + addWhippedTopping;
+        priceMessage = priceMessage + "\nAdd Chocolate Topping? " + addChocolateTopping;
+        priceMessage = priceMessage + "\nQuantity: " + quantity;
         priceMessage = priceMessage + "\nTotal: $" + price + " \nThank you!";
         return priceMessage;
     }
 
     public void increment(View view) {
-        Log.i("EnterpriseActivity.java", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
         quantity = quantity + 1;
         displayQuantity(quantity);
     }
@@ -89,11 +121,11 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given text on the screen.
      */
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        priceTextView.setText(message);
-
-
-    }
+//    private void displayMessage(String message) {
+//        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
+//        priceTextView.setText(message);
+//
+//
+//    }
 
 }
